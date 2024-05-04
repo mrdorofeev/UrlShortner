20dee901af5b9539848c15f99ea326eafd0d4745
Oleg Dorofeev <40496418+mrdorofeev@users.noreply.github.com>
2024-05-04 18:15:20 +0300

    Add files via upload

--- urlshortner/urlshortner/urlshortner/settings.py
+++ urlshortner/urlshortner/urlshortner/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '_nyq!4+7ya^5bq3x$_*o34q)doiv5rcxk2$b5fmla^x)rjz(7$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,10 +76,22 @@ WSGI_APPLICATION = 'urlshortner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'urls',
+        'USER': 'oleg',
+        'PASSWORD': 'oleg',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
